b6162ce8e2c9f8d3003f23a83f8c3abe0daf4d16
ufukincibas <ufukincibas@hotmail.com>
2024-10-18 02:49:07 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function App(){
+  return(
+    <View>
+      <Text>Hello Firebase!</Text>
+    </View>
+  )
+}
+
+export default App;
