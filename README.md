498cb112871b64f47ccc9cd05f59fe86957dbb33
ufukincibas <ufukincibas@hotmail.com>
2024-10-18 03:26:20 +0300

    Basic Authantication Module Structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Hello Firebase !!!
+
+Basic Authantication Modül Yapısı
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View , Button} from "react-native";
+import auth from '@react-native-firebase/auth';
 
 function App(){
+  const SignUp = () => {
+    auth().createUserWithEmailAndPassword(
+      'jane.doe@example.com', 
+      'SuperSecretPassword!')
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }
+
+  const SignIn = () => {
+    auth().signInWithEmailAndPassword(
+      'jane.doe@example.com', 
+      'SuperSecretPassword!')
+      .then(res => console.log("Signed in .."))
+      .catch(err => console.log(err))
+  }
+
+  const SignOut = () => {
+    auth().signOut()
+    .then(res => console.log("Sign Out.."))
+    .catch(err => console.log(err))
+  }
+
+  const CheckOut = ()  => {
+    const user = auth().currentUser;
+    console.log(user)
+
+
+  }
+
+
   return(
     <View>
       <Text>Hello Firebase!</Text>
+      <Button title="SignUp" onPress={SignUp}/>
+      <Button title="SignIn" onPress={SignIn}/>
+      <Button title="SignOut" onPress={SignOut}/>
+      <Button title="CheckOut" onPress={CheckOut}/>
     </View>
   )
 }
